749d2ea45ab2c7e820aad373d418193efdc0b8c0
Chad Lee <git@chadly.net>
2023-09-23 17:08:21 -0500

    tweak title

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🧠🌳 Idea Garden",
+    pageTitle: "🧠🌳",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
